cd36a5db7529d6ad76ed153a3f657e9e8d1ab6d2
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-03-22 12:00:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Example of Adding Newlines in PHP</title>
+    <meta charset="UTF-8">
+    <title>A Simple PHP File</title>
 </head>
 <body>
-
-
-<?php
-echo nl2br("Hello World!I've made a \r\n change to this !!");
-
-echo "Will this work properly ????";
-
-echo "This line was added 27/02/2019 @ 16:49";
-
-
-echo "If you view the source of output frame \r\n you will find a newline in this string.";
-echo "<br>";
-echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
-?>
-
+    <h1><?php echo "Dirty Leeds :-D "; ?></h1>
 </body>
-</html>   
+</html>
