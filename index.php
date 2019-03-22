44c05d04a706d7817e11923ce33201bb65f44a45
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-03-22 14:12:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <title>A Simple PHP File</title>
 </head>
-<body>
     .box {
     width: 300px;
     height: 150px;
@@ -12,7 +11,8 @@
     border: 2px solid #f08080;
     border-radius: 20px;
 } 
-    
+<body>
+        
     <h1><?php echo "Dirty Leeds :-D "; ?></h1>
 </body>
 </html>
