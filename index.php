1f2d7ae273730b6e923a41b9c1371cf1842e5417
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-03-22 11:56:23 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,9 @@ echo nl2br("Hello World!I've made a \r\n change to this !!");
 
 echo "Will this work properly ????";
 
-echo "This line was added 27/02/2019 @ 16:49"
+echo "This line was added 27/02/2019 @ 16:49" ?>
 
-
-
-
-
-
+<?php
 echo "If you view the source of output frame \r\n you will find a newline in this string.";
 echo "<br>";
 echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
