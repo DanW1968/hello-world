dd912fc4b3af6f2222e642c19ce2724805ac0f29
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-03-25 16:10:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>A Simple PHP File</title>
-<style type="text/css">  
-    .box {
-    width: 300px;
-    height: 150px;
-    background: #f1f442;
-    border: 2px solid #f08080;
-    border-radius: 20px;
-} 
-</style>
+    <title>Dirty Leeds !!</title>
 </head>
-   
 <body>
-    <div class="box"><h1><?php echo "Dirty Leeds :-D "; ?></h1></div>    
-    
+    <p>Hello World!<p>
 </body>
 </html>
