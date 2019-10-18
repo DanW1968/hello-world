45403497ac81a67ff11c98f2c3ffade187c9cfde
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-10-18 10:46:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
     <title>Danny's world famous Dirty Leeds Web App !!</title>
 </head>
 <body>
+    <?php
+// Defining Constant
+          define("App_Title", "The Dirty Leeds App");
+          echo 'Thanks for visiting ' . App_Title;
+    ?>
     <p><p>
     <marquee behavior="scroll" direction="left"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="right"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="left"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="right"><b>Dirty Leeds !!!!<b></marquee>
-<?php
-// Defining Constant
-          define("App_Title", "The Dirty Leeds App");
-          echo 'Thanks for visiting ' . App_Title;
-?>
+
         
 </body>
 </html>
