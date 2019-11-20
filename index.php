741cd8d954a0004b4fe8ac7e818af3f0c6bba11e
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-11-20 14:51:39 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
           echo 'Thanks for visiting ' . App_Title;
     ?>
     <p><p>
-    <marquee behavior="scroll" direction="left" scrollaount="1"><b>Dirty Leeds !!!!<b></marquee>
-    <marquee behavior="scroll" direction="right" scrollaount="5"><b>Dirty Leeds !!!!<b></marquee>
-    <marquee behavior="scroll" direction="left" scrollaount="10"><b>Dirty Leeds !!!!<b></marquee>
-    <marquee behavior="scroll" direction="right" scrollaount="3"><b>Dirty Leeds !!!!<b></marquee>
+    <marquee behavior="scroll" direction="left" scrollamount="1"><b>Dirty Leeds !!!!<b></marquee>
+    <marquee behavior="scroll" direction="right" scrollamount="5"><b>Dirty Leeds !!!!<b></marquee>
+    <marquee behavior="scroll" direction="left" scrollamount="10"><b>Dirty Leeds !!!!<b></marquee>
+    <marquee behavior="scroll" direction="right" scrollamount="3"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="up"><b>Dirty Leeds !!!!<b></marquee>
     <br />&nbsp;<br />
     <br />&nbsp;<br />
