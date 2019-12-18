c70aad3769d9dde661dc9ffc38306d06a0f40e11
DanW1968 <46194865+DanW1968@users.noreply.github.com>
2019-12-18 15:01:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
           echo 'Thanks for visiting ' . App_Title;
     ?>
     <p><p>
-    <marquee behavior="scroll" direction="left" scrollamount="1" style="color:DodgerBlue;"><h1>Dirty Leeds!!</h1></marquee>
+    <marquee behavior="scroll" direction="left" scrollamount="5" style="color:DodgerBlue;"><h1>Dirty Leeds!!</h1></marquee>
     <marquee behavior="scroll" direction="right" scrollamount="5" style="color:Tomato;"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="left" scrollamount="10" style="color:MediumSeaGreen;"><b>Dirty Leeds !!!!<b></marquee>
     <marquee behavior="scroll" direction="right" scrollamount="25" style="color:SlateBlue;"><b>Dirty Leeds !!!!<b></marquee>
