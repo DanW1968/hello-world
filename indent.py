49cf1920fe0cba5797495b8b53bd150f01112786
Danny Williams <danny.williams@igroupltd.co.uk>
2020-10-19 14:57:23 +0100

    if then else

---
+++ indent.py
@@ -0,0 +1,21 @@
+x=5
+if x> 2:
+    print('bigger than 2')
+    print('still bigger')
+print('done with 2')
+
+for i in range(5) :
+    print(i)
+    if  i>2 :
+        print('bigger than 2')
+    print('done with i' , i)
+print('all done')
+
+x = 4
+if x > 2:
+    print('Bigger')
+else :
+    print('smaller')
+
+print('All done')
+
