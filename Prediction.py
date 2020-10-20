b36bdec58d4496884318ab52b75af3c49eb380c0
Danny Williams <danny.williams@igroupltd.co.uk>
2020-10-20 13:17:33 +0100

    .

---
+++ Prediction.py
@@ -0,0 +1,9 @@
+#Score Prediciton Programme
+#Could be a while befdre there is anything useful
+
+print('Welcome')
+print('What do you need ?')
+
+#import results and update DB
+#get fixtures
+#calculate predictions
