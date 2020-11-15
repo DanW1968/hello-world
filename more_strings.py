9fdabf78df2279b72cce0e95a27a42a476d125f1
Danny Williams <danny.williams@igroupltd.co.uk>
2020-11-15 21:55:14 +0000

    na

--- more_strings.py
+++ more_strings.py
@@ -3,3 +3,29 @@ zap = greet.lower()
 print(zap)
 print(greet)
 print('Hi There'.lower())
+
+
+fruit = 'banana'
+pos = fruit.find('na')
+print(pos)
+
+aa=fruit.find('z')
+print(aa)
+
+
+greet = 'Hello Bob'
+nstr = greet.replace('Bob','Jane')
+print(nstr)
+
+nstr = greet.replace('o','x')
+print(nstr)
+
+data = 'From stephen.marquard@uct.ac.za Sat Jan   5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)
+
+sppos = data.find(' ',atpos)
+print(sppos)
+
+host = data[atpos+1 : sppos]
+print(host)
