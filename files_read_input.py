3c0352be5fb6631d1f1247a8f08b73d496c5dcff
Danny Williams <danny.williams@igroupltd.co.uk>
2020-11-16 22:01:44 +0000

    na

--- files_read_input.py
+++ files_read_input.py
@@ -1,5 +1,15 @@
-fhand = open("c:\Src1\hello-world/mbox-short.txt")
+fhand = open("c:/Src1/hello-world/mbox-short.txt")
 for line in fhand :
     line = line.rstrip()
     if line.startswith('From:') :
         print(line)
+
+
+
+fname = input('Enter the file name : ')
+fhand = open(fname)
+count = 0
+for line in fhand : 
+    if line.startswith('Subject:') :
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
